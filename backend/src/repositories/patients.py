27e398a7e397bf326f89bf7c0734c82f4ffcf5a4
Okhrymenko <Ohrimenko15@gmail.com>
2026-07-05 19:23:06 +0300

    refactor: rename Patient to PatientModel

--- backend/src/repositories/patients.py
+++ backend/src/repositories/patients.py
@@ -1,26 +1,26 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import Patient
+from database import PatientModel
 
 
 class PatientRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_by_id(self, patient_id: int) -> Patient | None:
+    async def get_by_id(self, patient_id: int) -> PatientModel | None:
         return await self.session.scalar(
-            select(Patient).where(Patient.patient_id == patient_id)
+            select(PatientModel).where(PatientModel.patient_id == patient_id)
         )
 
-    async def get_all(self) -> list[Patient]:
-        result = await self.session.scalars(select(Patient))
+    async def get_all(self) -> list[PatientModel]:
+        result = await self.session.scalars(select(PatientModel))
         return list(result.all())
 
-    def add_patient(self, patient: Patient) -> None:
+    def add_patient(self, patient: PatientModel) -> None:
         self.session.add(patient)
 
-    async def delete_patient(self, patient: Patient) -> None:
+    async def delete_patient(self, patient: PatientModel) -> None:
         await self.session.delete(patient)
 
     async def flush(self) -> None:
